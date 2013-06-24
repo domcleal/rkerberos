b0391c902e9443a197b7e0378f7228154261221b
Dominic Cleal <dcleal@redhat.com>
2013-06-24 23:10:54 +0100

    Fix kadm5clnt build issue on EL6
    Explicitly require com_err to populate the include path and fix kadm5/admin.h
    error on EL6 with e2fsprogs 1.41, which doesn't have /usr/include/com_err.h.

--- ext/rkerberos/extconf.rb
+++ ext/rkerberos/extconf.rb
@@ -5,6 +5,10 @@ dir_config('rkerberos', '/usr/local')
 have_header('krb5.h')
 have_library('krb5')
 
+unless pkg_config('com_err')
+  puts 'warning: com_err not found, usually a dependency for kadm5clnt'
+end
+
 if have_header('kadm5/admin.h')
   have_library('kadm5clnt')
 else
